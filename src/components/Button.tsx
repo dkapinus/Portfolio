3538074317f5f21fffeb138fe081d2f87a4effdf
dkapinus <dimonio@tut.by>
2023-11-21 12:09:24 +0300

    Main Part2

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,9 +3,21 @@ import styled from "styled-components";
 import {Theme} from "styles/Theme";
 
 export const Button = styled.button`
+
+  color: #252728;
+
+  text-align: center;
+  font-size: 16px;
+  font-weight: 500;
+  
+  
   border-radius: 6px;
   background:${Theme.colors.accent};
   padding: 12px 20px 13px 20px;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
 
   :hover {
     cursor: pointer;
--- src/layaot/section/main/Main.tsx
+++ src/layaot/section/main/Main.tsx
@@ -2,53 +2,68 @@ import React from 'react';
 import styled from "styled-components";
 import mayPhoto from "assets/images/photo_2023-10-16-15.23.22-_1_ (1).png"
 import {FlexWrapper} from "components/FlexWrapper";
-import {Theme} from "styles/Theme";
 import {Container} from "components/container/Container";
 import {Button} from "components/Button";
+import {Theme} from "styles/Theme";
 
 export const Main = () => {
 
     return (
         <StyledMain>
-           <Container>
-               <FlexWrapper align={"center"} justify={"space_around"} >
-                   <div>
-                       <MainTitle>WEB DEVELOPER</MainTitle>
-                       <Name>John Doe</Name>
-                       <MainText>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
-                           Velit officia consequat duis enim velit mollit. Exercitation veniam consequat
-                           sunt.</MainText>
-                       <Button>Contact Me</Button>
-                   </div>
+            <Container>
+                <FlexWrapper align={"center"} justify={"space-between"}>
+                    <div>
+                        <MainTitle>WEB DEVELOPER</MainTitle>
+                        <Name>Kapinus Dima</Name>
+                        <MainText>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
+                            Velit officia consequat duis enim velit mollit. Exercitation veniam consequat
+                            sunt.</MainText>
+                        <Button>Contact Me</Button>
+                    </div>
 
-                   <MainImage src={mayPhoto} alt=""/>
-               </FlexWrapper>
-           </Container>
+                    <MainImage src={mayPhoto} alt=""/>
+                </FlexWrapper>
+            </Container>
         </StyledMain>
     );
 };
 
 
 const StyledMain = styled.section`
- 
   min-height: 100vh;
- 
+  display: flex;
+
 `
 
-const MainImage =styled.img`
+const MainImage = styled.img`
   width: 350px;
   height: 430px;
   object-fit: cover;
 `
 
-const MainTitle =styled.h1 `
-    
+const MainTitle = styled.h1`
+  color: #D7E5EC;
+  font-family: Tinos, sans-serif;
+  font-size: 20px;
+  font-weight: 400;
+
 `
 
-const Name =styled.h2`
+const Name = styled.h2`
+  font-size: 72px;
+  font-weight: 600;
+  background: var(--gradient, linear-gradient(90deg, #00F5A0 0%, #00D9F5 100%));
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+
 `
 
-const MainText =styled.p`
+const MainText = styled.p`
+  max-width: 514px;
+  color: ${Theme.colors.font};
+  font-size: 16px;
+  font-weight: 400;
+  margin: 20px 0;
 `
 
 
